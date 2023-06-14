5e048ad39540441fad22f4a3ece0b74e56de876d
yasmineismail <yasmineiismail@gmail.com>
2023-06-14 17:45:49 +0000

    mandatory tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the result of addition of all arguments."""
+    import sys
+
+    t = 0
+    for i in range(len(sys.argv) - 1):
+        t += int(sys.argv[i + 1])
+    print("{}".format(t))
