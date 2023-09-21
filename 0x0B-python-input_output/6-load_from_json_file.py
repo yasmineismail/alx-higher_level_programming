1e6eae0260c29adf6c22125979a089781301db34
yasmineismail <114994646+yasmineismail@users.noreply.github.com>
2023-09-21 22:45:52 +0400

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Creates an Object from a JSON file"""
-import json
-
-
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
--- 0x0B-python-input_output/README.md
+++
@@ -1,2 +0,0 @@
-Python - Input/Output
-
